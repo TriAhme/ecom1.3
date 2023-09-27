b295a4484ff9e4a65aafaa2e94d5210c028b3b75
trigui ahmed <trigui.ahmed11@gmail.com>
2023-09-27 17:56:23 -0400

    add form and result page

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ecom1.3
+
+commerce electronique
+cours 3
+
+## partie 1
+
+- item 1
+- item 2
+- item 3
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="result.php">
+Name:
+<input type="text" name="frame">
+<input type="submit">
+</form>  
+
+</body>
+</html>
